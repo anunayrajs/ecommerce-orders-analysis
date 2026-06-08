61dd006004be68fd50f2a688c3d22895ed8ab14e
ANUNAY RAJ <anunayraj999@gmail.com>
2026-06-09 01:01:23 +0530

    completed level 1 exploration queries

--- sql/01_exploration.sql
+++ sql/01_exploration.sql
@@ -13,3 +13,28 @@ SELECT 'order_reviews',                              COUNT(*)               FROM
 SELECT 'products',                                   COUNT(*)               FROM products                      UNION ALL
 SELECT 'sellers',                                    COUNT(*)               FROM sellers                       UNION ALL
 SELECT 'product_category_translation', COUNT(*)                             FROM product_category_translation;
+
+
+-- Q1. How many total orders are in the dataset?
+SELECT COUNT(*) AS total_orders FROM orders;
+
+-- Q2. What is the date range of orders — earliest and latest order purchase date?
+SELECT MIN(order_purchase_timestamp) AS earliest_purchase_date, MAX(order_purchase_timestamp) AS latest_purchase_date
+FROM orders;
+
+-- Q3. How many unique customers, sellers and product categories are there?
+SELECT
+(SELECT COUNT(DISTINCT(customer_id)) FROM customers) AS unique_customers,
+(SELECT COUNT(DISTINCT(seller_id )) FROM sellers) AS unique_sellers ,
+(SELECT COUNT(DISTINCT(product_category_name)) FROM products) AS unique_product_category;
+
+-- Q4. What is the order status breakdown — how many orders per status?
+SELECT order_status, COUNT(*) AS orders_per_status FROM orders
+GROUP BY order_status
+ORDER BY orders_per_status DESC;
+
+-- Q5. Which are the top 10 states by number of customers?
+SELECT customer_state, COUNT(*) AS total_cusotmers FROM customers
+GROUP BY customer_state
+ORDER BY total_cusotmers DESC
+LIMIT 10;
