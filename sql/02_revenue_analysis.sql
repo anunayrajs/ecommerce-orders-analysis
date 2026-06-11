ef951297296289198fbe5964650468e52e23870f
ANUNAY RAJ <anunayraj999@gmail.com>
2026-06-12 00:18:19 +0530

    completed level 2 revenue analysis

--- sql/02_revenue_analysis.sql
+++ sql/02_revenue_analysis.sql
@@ -31,3 +31,29 @@ INNER JOIN product_category_translation t
 GROUP BY t.product_category_name_english
 ORDER BY total_revenue DESC
 LIMIT 10;
+
+-- Q10. Which months have revenue above the overall monthly average? (use HAVING)
+SELECT
+	TO_CHAR(o.order_purchase_timestamp, 'YYYY-MM') AS year_month,
+	ROUND(SUM(p.payment_value)::NUMERIC,2) AS monthly_revenue
+FROM orders o
+INNER JOIN order_payments p
+	ON o.order_id = p.order_id
+GROUP BY TO_CHAR(o.order_purchase_timestamp,'YYYY-MM')
+HAVING SUM(p.payment_value) > (SELECT AVG(monthly_total) 
+	FROM (
+		SELECT SUM(p2.payment_value) AS monthly_total
+		FROM orders o2
+		INNER JOIN order_payments p2
+			ON o2.order_id = p2.order_id
+		GROUP BY TO_CHAR(o2.order_purchase_timestamp,'YYYY-MM')
+	) AS subquery 
+)
+ORDER BY year_month;
+
+-- Q11. What is the % revenue contribution of each payment type? (credit_card, boleto, voucher, debit_card)
+SELECT payment_type,
+	ROUND(SUM(payment_value)::NUMERIC,2) AS type_revenue,
+	ROUND(SUM(payment_value) *100 / SUM(SUM(payment_value)) OVER() ::NUMERIC,2) AS pct_contribution
+FROM order_payments
+GROUP BY payment_type;
